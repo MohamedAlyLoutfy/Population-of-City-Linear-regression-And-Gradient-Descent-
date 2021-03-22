81e90850b1db8572525f44b2651a7f21e3904bfe
MohamedAlyLoutfy <73834061+MohamedAlyLoutfy@users.noreply.github.com>
2021-03-22 15:36:43 +0200

    Update ex1.m

--- ex1.m
+++ ex1.m
@@ -35,20 +35,18 @@ alpha = 0.01;
 
 computeCost(X, y, theta)
 
-% run gradient descent
+
 theta = gradientDescent(X, y, theta, alpha, iterations);
 
-% print theta to screen
+
 fprintf('Theta found by gradient descent: ');
 fprintf('%f %f \n', theta(1), theta(2));
 
-% Plot the linear fit
-hold on; % keep previous plot visible
+hold on; 
 plot(X(:,2), X*theta, '-')
 legend('Training data', 'Linear regression')
 hold off % don't overlay any more plots on this figure
 
-% Predict values for population sizes of 35,000 and 70,000
 predict1 = [1, 3.5] *theta;
 fprintf('For population = 35,000, we predict a profit of %f\n',...
     predict1*10000);
@@ -59,17 +57,15 @@ fprintf('For population = 70,000, we predict a profit of %f\n',...
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-%% ============= Part 4: Visualizing J(theta_0, theta_1) =============
 fprintf('Visualizing J(theta_0, theta_1) ...\n')
 
-% Grid over which we will calculate J
 theta0_vals = linspace(-10, 10, 100);
 theta1_vals = linspace(-1, 4, 100);
 
-% initialize J_vals to a matrix of 0's
+
 J_vals = zeros(length(theta0_vals), length(theta1_vals));
 
-% Fill out J_vals
+
 for i = 1:length(theta0_vals)
     for j = 1:length(theta1_vals)
 	  t = [theta0_vals(i); theta1_vals(j)];    
@@ -77,18 +73,14 @@ for i = 1:length(theta0_vals)
     end
 end
 
-
-% Because of the way meshgrids work in the surf command, we need to 
-% transpose J_vals before calling surf, or else the axes will be flipped
 J_vals = J_vals';
-% Surface plot
+
 figure;
 surf(theta0_vals, theta1_vals, J_vals)
 xlabel('\theta_0'); ylabel('\theta_1');
 
-% Contour plot
 figure;
-% Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+
 contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
 xlabel('\theta_0'); ylabel('\theta_1');
 hold on;
